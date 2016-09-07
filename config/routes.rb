5f168e59924e89f4d633269374906625f9641c5a
Ora-Kool <etingemabian@gmail.com>
2016-09-07 09:14:23 +0100

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
   resources :patients
+  root 'patients#index'
 
   get '/remotappoint/about', to: 'patients#about'
   get '/remotappoint/how-it-works', to: 'patients#how-it-works'
   get '/remotappoint/health-tips', to: 'patients#health-tips'
+  get '/remotappoint/terms-of-use', to: 'patients#terms'
+  get '/remotappoint/privacy-policy', to: 'patients#privacy'
   get '/remotappoint/news', to: 'patients#news'
-  root 'patients#index'
+  get '/remotappoint/new', to: 'patients#new'
+
+  
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
